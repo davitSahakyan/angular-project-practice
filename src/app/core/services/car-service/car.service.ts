813281edf65795e44e2a27640e72e4d815f84477
Davit Sahakyan <davit.sahakyan@org.fcc.am>
2020-10-05 17:38:59 +0400

    Add car service

--- src/app/core/services/car-service/car.service.ts
+++ src/app/core/services/car-service/car.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CarService {
-  constructor() {}
+export class CarService implements OnInit {
+  data: any;
 
-  consoleSomething() {
-    console.log('wefwe');
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  fetchCars() {
+    return this.http.get('https://car-list-7a458.firebaseio.com/cars.json');
+  }
+
+  addCar() {
+    this.http
+      .post('https://car-list-7a458.firebaseio.com/cars.json', {
+        h: 'ewrf',
+      })
+      .subscribe();
+  }
+
+  getData() {
+    return this.data;
   }
 }
